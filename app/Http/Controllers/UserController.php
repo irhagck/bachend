78bd8d4738536fa276b6e877a1031db6e0023f6e
info@gck.com <info@gck.com>
2025-12-10 10:31:52 +0500

    it's done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,10 +18,7 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    
 
     /**
      * Store a newly created resource in storage.
